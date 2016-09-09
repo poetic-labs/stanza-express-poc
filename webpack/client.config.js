680172a012eaa9b9f08f48ad9aeb72f1fb8f512f
John Rodney <jrodney@poeticsystems.com>
2016-09-09 13:10:35 -0400

    remove pointless files

--- webpack/client.config.js
+++
@@ -1,6 +0,0 @@
-const webpackConfigFactory = require('./configFactory');
-
-module.exports = function clientConfigFactory(options = {}, args = {}) {
-  const { mode = 'development' } = options;
-  return webpackConfigFactory({ target: 'client', mode }, args);
-};
--- webpack/server.config.js
+++
@@ -1,6 +0,0 @@
-const webpackConfigFactory = require('./configFactory');
-
-module.exports = function serverConfigFactory(options = {}, args = {}) {
-  const { mode = 'development' } = options;
-  return webpackConfigFactory({ target: 'server', mode }, args);
-};
